8ad1bb668b4fb04631bf11b49f2640b9a408ee9b
hkt <532131191@qq.com>
2020-07-12 17:27:36 +0800

    基础jQuery案例练习

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,27 +1,33 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
 /**
  * @author Administrator
  */
+@RequestMapping("/test")
 @Controller
 public class UserController {
-    @GetMapping("/test")
-   // @ResponseBody
-    public Map<String,Object> test(){
-        Map<String, Object> ret = new HashMap<String, Object>();
+    @GetMapping("/test1")
+    //@ResponseBody
+    public String test(Model model){
+        //Map<String, Object> ret = new HashMap<String, Object>();
         User user=new User();
         user.setName("hh");
         user.setAge(33);
-        ret.put("user",user);
-
-        return ret;
+       // ret.put("user",user);
+        model.addAttribute("user",user);
+        return "test";
     }
+    @GetMapping("/test2")
     public String test2(){
 
         return "success";
